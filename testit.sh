c6b51339abeb5a217dafea1791c9279b8e170a39
Pathway <peter.wilks@pathway-technologies.com>
2021-02-25 08:37:57 +0000

    Update testit.sh

--- testit.sh
+++ testit.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 echo Test script for the sphinx-MiKTeX docker container
+
+sphinx-build --version
+
+# Make a simple Sphinx workspace
+md SphinxTest
+cd SphinxTest
+sphinx-quickstart -q -p MyProject -a Snoopy -v 0.1
+
+ls -l
