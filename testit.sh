5b4a840f04e2ad37fe5b1d5f1ab34aac6a06586e
Pathway <peter.wilks@pathway-technologies.com>
2021-02-25 09:22:56 +0000

    Update testit.sh

--- testit.sh
+++ testit.sh
@@ -4,10 +4,10 @@ echo Test script for the sphinx-MiKTeX docker container
 sphinx-build --version
 
 ######
-apt-get install gnupg
+apt install -y gnupg
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
 echo "deb http://miktex.org/download/ubuntu focal universe" | tee /etc/apt/sources.list.d/miktex.list
-apt-get install miktex
+apt install -y miktex
 ######
 
 
