3d2f388966ba1ff6aad76436868e13edc2e55cb0
Pathway <peter.wilks@pathway-technologies.com>
2021-02-25 09:40:26 +0000

    Update testit.sh

--- testit.sh
+++ testit.sh
@@ -3,17 +3,8 @@ echo Test script for the sphinx-MiKTeX docker container
 
 sphinx-build --version
 
-######
-apt install -y gnupg
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
-echo "deb http://miktex.org/download/ubuntu focal universe" | tee /etc/apt/sources.list.d/miktex.list
-apt-get update
-apt install -y miktex
-######
-
-
 # Make a simple Sphinx workspace and build for latex
 mkdir SphinxTest
 cd SphinxTest
 sphinx-quickstart -q -p MyProject -a Snoopy -v 0.1
-make latex
+make latexpdf
