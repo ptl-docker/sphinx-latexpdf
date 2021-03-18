abc5331a69e09ecc33c182a05454f11f0f32bf26
Pathway <peter.wilks@pathway-technologies.com>
2021-03-18 20:33:34 +0000

    Update testit.sh

--- testit.sh
+++ testit.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-echo Test script for the sphinx-MiKTeX docker container
+echo Test script for the sphinx-texlive docker container
 
 # Get installed version numbers
 sphinx-build --version
+tex --version
 
 # Make a simple Sphinx workspace and build for latex
 mkdir SphinxTest
 cd SphinxTest
 sphinx-quickstart -q -p MyProject -a Snoopy -v 0.1
 make latexpdf
+
+#####TESTTEST
+find /usr/share -name *.sty
+#####TESTTEST
 
 # Artefacts
 find . -name *.pdf
