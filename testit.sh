90393a45d0d5c345635015c04d329ab977701a08
Pathway <peter.wilks@pathway-technologies.com>
2021-03-19 20:11:19 +0000

    Update testit.sh

--- testit.sh
+++ testit.sh
@@ -6,7 +6,7 @@ sphinx-build --version
 tex --version
 
 # install missing packages
-tlmgr install fncychap titlesec tabulary varwidth
+tlmgr install fncychap titlesec tabulary varwidth framed
 
 # Make a simple Sphinx workspace and build for latex
 mkdir SphinxTest
