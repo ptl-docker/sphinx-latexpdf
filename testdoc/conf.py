79d0ad4d5c86f39e9e1554890bec4e42be3e9f79
Peter Wilks <peter.wilks@pathway-technologies.com>
2021-04-22 09:18:36 +0200

    paths

--- testdoc/conf.py
+++ testdoc/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/_assets'))
+sys.path.insert(0, os.path.abspath('_assets'))
 
 
 # -- Project information -----------------------------------------------------
